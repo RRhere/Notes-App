97a142c8bf63be8db4ea8e158c37fac580e12e00
RRhere <ragavradhesh@gmail.com>
2026-05-09 21:50:57 +0530

    Initialize Flask-Mail in the application factory

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,9 @@ from flask_wtf.csrf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_talisman import Talisman
+from flask_mail import Mail
 
+mail = Mail()
 db = SQLAlchemy()
 csrf = CSRFProtect()
 limiter = Limiter(key_func=get_remote_address)
@@ -47,6 +49,7 @@ def create_app():
     db.init_app(app)
     csrf.init_app(app)
     limiter.init_app(app)
+    mail.init_app(app)
     
     # Add security headers with Flask-Talisman
     Talisman(
